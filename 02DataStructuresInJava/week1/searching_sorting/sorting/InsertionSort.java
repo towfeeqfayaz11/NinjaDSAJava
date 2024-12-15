3d64945f56c0788027865de779bfde5678a2d1b7
towfeeqfayaz11 <towfeeqpandith@gmail.com>
2024-12-16 05:16:05 +0900

    selection sort

---
+++ 02DataStructuresInJava/week1/searching_sorting/sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+/*
+Intution for insertion sort:
+
+*/
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        insertionSort(arr);
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
---
+++ 02DataStructuresInJava/week1/searching_sorting/sorting/SelectionSort.java
@@ -0,0 +1,35 @@
+/*
+Intution for selection sort:
+1. Find the minimum element in the array and swap it with the element at the beginning of the array.
+2. Repeat the above process for the remaining elements of the array.
+3. Time Complexity: O(n^2)
+4. Space Complexity: O(1)
+
+ideal to use when input is very small and simple
+*/
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        selectionSort(arr);
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+}
